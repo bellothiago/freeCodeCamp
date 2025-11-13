9fb6203e471b222db2e6523800b181097f1f1662
Thiago Bello <thiagolbello@gmail.com>
2025-11-13 18:16:22 +0100

    Added sixtieth project: "Build a JavaScript Trivia Bot"

---
+++ Certified Full Stack Developer Curriculum/JavaScript/JavaScript_Trivia_Bot/main.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "tars";
+let botLocation = "Saturn";
+let favoriteLanguage = "Python";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Fun fact: " + favoriteLanguage + " is also a snake.";
+
+console.log(codingFact);
+
+codingFact = "But... both " + favoriteLanguage + "s can be difficult to comprehend..."
+
+console.log(codingFact);
+
+codingFact = "However, that makes both " + favoriteLanguage + "s beautiful too!";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
