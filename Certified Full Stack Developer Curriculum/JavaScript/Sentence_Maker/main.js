9f62415bf7fc75ed93d312fbba869cb2fb2b8f1b
Thiago Bello <thiagolbello@gmail.com>
2025-11-14 19:16:09 +0100

    Added sixty-first project: "Build a Sentence Maker"

---
+++ Certified Full Stack Developer Curriculum/JavaScript/Sentence_Maker/main.js
@@ -0,0 +1,21 @@
+let adjective = "pink";
+let noun = "tiger";
+let verb = "running";
+let place = "hill";
+let adjective2 = "fiery";
+let noun2 = "panther";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 +  ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "golden";
+noun = "chicken";
+verb = "floating";
+place = "rocket ship";
+adjective2 = "big";
+noun2 = "beetroot";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 +  ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
